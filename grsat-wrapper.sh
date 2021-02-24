657138b8dfd8a87355bfadd42b32c851e68091c1
Daniel Ekman <knegge@gmail.com>
2021-02-24 18:41:36 +0100

    Removed audio mode and shellchecked a bit

--- grsat-wrapper.sh
+++ grsat-wrapper.sh
@@ -4,7 +4,7 @@ export PYTHONPATH=/usr/local/lib/python3/dist-packages/
 
 CMD="$1"     # $1 [start|stop]
 ID="$2"      # $2 observation ID
-FREQ="$3"    # $3 frequency
+#FREQ="$3"    # $3 frequency
 TLE="$4"     # $4 used tle's
 DATE="$5"    # $5 timestamp Y-m-dTH-M-S
 BAUD="$6"    # $6 baudrate
@@ -14,7 +14,7 @@ PRG="gr-satellites:"
 TMP="/tmp/.satnogs"
 SATLIST="$TMP/grsat_list.txt"  # SATNOGS_APP_PATH
 GRSVER="$TMP/grsat_list.ver"
-GRSBIN=$(which gr_satellites)
+GRSBIN=$(command -v gr_satellites)
 GRSTS="stat -c %Y $GRSBIN"
 LOG="$TMP/grsat_$ID.log"
 KSS="$TMP/grsat_$ID.kss"
@@ -23,9 +23,6 @@ DATA="$TMP/data"   # SATNOGS_OUTPUT_PATH
 
 # Settings
 KEEPLOGS=no       # yes = keep, all other = remove KSS LOG
-
-# if IQ mode set, then you also need find_samp_rate.py
-IQMODE=no          # yes = use IQ UDP data, all other = use audio UDP data
 
 # uncomment and populate SELECTED with space separated norad id's
 # to selectively submit data to the network
@@ -40,19 +37,15 @@ SATNAME=$(echo "$TLE" | jq .tle0 | sed -e 's/ /_/g' | sed -e 's/[^A-Za-z0-9._-]/
 NORAD=$(echo "$TLE" | jq .tle2 | awk '{print $2}')
 echo "$PRG Observation: $ID, Norad: $NORAD, Name: $SATNAME, Script: $SCRIPT"
 
-if [[ $UDP_DUMP_PORT == ?(-)+([0-9]) ]]; then
-        AUDIO_PORT=$UDP_DUMP_PORT
-        IQ_PORT=$((UDP_DUMP_PORT + 1))
-else
-        AUDIO_PORT=7355
-        IQ_PORT=7356
-fi
-
-if [ -z ${UDP_DUMP_HOST} ]; then
+if [ -z "$UDP_DUMP_HOST" ]; then
 	echo "Warning: UDP_DUMP_HOST not set, no data will be sent to the demod"
 fi
 
-if [ ${CMD^^} == "START" ]; then
+if [ -z "$UDP_DUMP_PORT" ]; then
+        UDP_DUMP_PORT=57356
+fi
+
+if [ "${CMD^^}" == "START" ]; then
   if [ ! -f "$SATLIST" ] || [ ! -f "$GRSVER" ] || [ "$($GRSTS)" != "$(<"$GRSVER")" ]; then
     echo "$PRG Generating satellite list"
     $GRSBIN --list_satellites | sed  -n -Ee  's/.*NORAD[^0-9]([0-9]+).*/\1/p' > "$SATLIST"
@@ -61,19 +54,13 @@ if [ ${CMD^^} == "START" ]; then
 
   if grep -Fxq "$NORAD" "$SATLIST"; then
     echo "$PRG Starting observation $ID"
-    if [ ${IQMODE^^} == "YES" ]; then
-      SAMP=`find_samp_rate.py $BAUD $SCRIPT`
-      if [ -z ${SAMP} ]; then  # default 48k if script not found
-        SAMP=48000
-        echo "$PRG WARNING: find_samp_rate.py did not return valid sample rate!"
-      fi
-      GROPT="$NORAD --samp_rate $SAMP --iq --throttle --udp --udp_port $IQ_PORT --udp_raw --start_time $DATEF --kiss_out $KSS --ignore_unknown_args --use_agc"
-      echo "$PRG running in IQ mode at $SAMP sps"
-    else
+    SAMP=$(find_samp_rate.py "$BAUD" "$SCRIPT")
+    if [ -z "$SAMP" ]; then  # default 48k if script not found
       SAMP=48000
-      GROPT="$NORAD --samp_rate $SAMP --throttle --udp --udp_port $AUDIO_PORT --start_time $DATEF --kiss_out $KSS --ignore_unknown_args --f_offset 12000"  # --clk_limit 0.03
-      echo "$PRG running in audio mode at $SAMP sps"
+      echo "$PRG WARNING: find_samp_rate.py did not return valid sample rate!"
     fi
+    GROPT="$NORAD --samp_rate $SAMP --iq --throttle --udp --udp_port $UDP_DUMP_PORT --udp_raw --start_time $DATEF --kiss_out $KSS --ignore_unknown_args --use_agc"
+    echo "$PRG running at $SAMP sps"
     $GRSBIN $GROPT > "$LOG" 2>> "$LOG" &
     echo $! > "$GRPID"
   else
@@ -81,10 +68,10 @@ if [ ${CMD^^} == "START" ]; then
   fi
 fi
 
-if [ ${CMD^^} == "STOP" ]; then
+if [ "${CMD^^}" == "STOP" ]; then
   if [ -f "$GRPID" ]; then
     echo "$PRG Stopping observation $ID"
-    kill `cat "$GRPID"`
+    kill "$(cat "$GRPID")"
     rm -f "$GRPID"
   fi
 
@@ -109,7 +96,7 @@ if [ ${CMD^^} == "STOP" ]; then
     rm -f "$LOG"
   fi
 
-  if [ ${KEEPLOGS^^} == "YES" ]; then
+  if [ "${KEEPLOGS^^}" == "YES" ]; then
     echo "$PRG Keeping logs, you need to purge them manually."
   else
     rm -f "$LOG" "$KSS"
