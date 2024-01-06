255c2224812678d6c60b01a58840608e4ec5a90b
Daniel Ekman <knegge@gmail.com>
2024-01-06 13:46:21 +0100

    Add ssb calculation, pep code
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

--- find_samp_rate.py
+++ find_samp_rate.py
@@ -7,10 +7,10 @@
 import satnogs
 import sys
 
-sps=4
-audio_samp_rate=48000
-baudrate=9600
-script='satnogs_fm.py'
+sps = 4
+audio_samp_rate = 48000
+baudrate = 9600
+script = 'satnogs_fm.py'
 
 if len(sys.argv) == 2 or len(sys.argv) == 3:
     try:
@@ -23,16 +23,17 @@ if len(sys.argv) == 3:
 if baudrate < 1 or baudrate > 1e6:  # unknown (?)
     baudrate = 9600
 
-if   '_bpsk' in script:
-    print(satnogs.find_decimation(baudrate, 2, audio_samp_rate,sps)*baudrate)
+if '_bpsk' in script:
+    print(satnogs.find_decimation(baudrate, 2, audio_samp_rate, sps) * baudrate)
 elif '_fsk' in script:
-    print(max(4,satnogs.find_decimation(baudrate, 2, audio_samp_rate))*baudrate)
+    print(max(4, satnogs.find_decimation(baudrate, 2, audio_samp_rate)) * baudrate)
 elif '_sstv' in script:
     print(4*4160*4)
 elif '_qubik' in script:
-    print(max(4,satnogs.find_decimation(baudrate, 2, audio_samp_rate))*baudrate)
+    print(max(4, satnogs.find_decimation(baudrate, 2, audio_samp_rate)) * baudrate)
 elif '_apt' in script:
     print(4*4160*4)
+elif "_ssb" in script:
+    print(satnogs.find_decimation(baudrate, 2, audio_samp_rate, sps) * baudrate)
 else:  # cw, fm, afsk, etc...
     print(audio_samp_rate)
-
